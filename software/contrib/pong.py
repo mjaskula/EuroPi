682277029bd1f824c9a9df8477afec1d76f62e97
Matthew Jaskula <mjaskula@gmail.com>
2022-03-12 22:52:38 -0500

    WIP add collision detection

--- software/contrib/pong.py
+++ software/contrib/pong.py
@@ -5,25 +5,31 @@ from europi import OLED_HEIGHT, OLED_WIDTH, k1, k2, oled, MAX_OUTPUT_VOLTAGE
 from europi_script import EuroPiScript
 
 MAX_SCORE = int(MAX_OUTPUT_VOLTAGE)
+PADDLE_HEIGHT = 8
 
 
 class Paddle:
     def __init__(self, knob, x_pos, height) -> None:
         self.knob = knob
-        self.x_pos = x_pos
-        self.height = height
+        self.x = x_pos
+        self.half_height = int(height / 2)
+
+    @property
+    def y(self):
+        return self.knob.read_position(OLED_HEIGHT)
 
     def draw(self, oled):
-        y_pos = self.knob.read_position(OLED_HEIGHT - self.height)
-        oled.vline(self.x_pos, y_pos, self.height, 1)
+        oled.vline(self.x, self.y - self.half_height, self.half_height * 2, 1)
+
+    def hit(self, ball):
+        y = self.y
+        return ball.y >= (y - int(self.half_height)) and ball.y <= (y + int(self.half_height))
 
 
 class Ball:
-    def __init__(self, x, y) -> None:
-        self.start_x = x
-        self.start_y = y
-        self.x = x
-        self.y = y
+    def __init__(self) -> None:
+        self.start_x = int(OLED_WIDTH / 2)
+        self.start_y = int(OLED_HEIGHT / 2)
         self.reset()
 
     def reset(self):
@@ -50,6 +56,7 @@ class Score:
     def __init__(self) -> None:
         self.l = 0
         self.r = 0
+        self.x = int(OLED_WIDTH / 2) - 12
 
     def score_l(self):
         self.l += 1
@@ -58,7 +65,7 @@ class Score:
         self.r += 1
 
     def draw(self, oled):
-        oled.text(f"{self.l} {self.r}", int(OLED_WIDTH / 2) - 12, 0, 1)
+        oled.text(f"{self.l} {self.r}", self.x, 0, 1)
 
     def game_over(self):
         if self.l == MAX_SCORE or self.r == MAX_SCORE:
@@ -80,9 +87,9 @@ class Pong(EuroPiScript):
 
     def main(self):
         score = Score()
-        l_paddle = Paddle(k1, 0, 8)
-        r_paddle = Paddle(k2, OLED_WIDTH - 1, 8)
-        ball = Ball(int(OLED_WIDTH / 2), int(OLED_HEIGHT / 2))
+        l_paddle = Paddle(k1, 0, PADDLE_HEIGHT)
+        r_paddle = Paddle(k2, OLED_WIDTH - 1, PADDLE_HEIGHT)
+        ball = Ball()
         while True:
             # update game state
             oled.fill(0)
@@ -94,12 +101,14 @@ class Pong(EuroPiScript):
             self.draw_center_line(oled)
 
             if ball.x < 1:
-                score.score_l()
-                ball.reset()
+                if not l_paddle.hit(ball):
+                    score.score_l()
+                    ball.reset()
 
             if ball.x > OLED_WIDTH - 1:
-                score.score_r()
-                ball.reset()
+                if not r_paddle.hit(ball):
+                    score.score_r()
+                    ball.reset()
 
             if score.game_over():
                 ball.reset()
@@ -107,7 +116,6 @@ class Pong(EuroPiScript):
             # update CV output
 
             oled.show()
-            # sleep(0.1)
 
 
 if __name__ == "__main__":
